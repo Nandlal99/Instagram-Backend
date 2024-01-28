645668adcf36f33c0364c8f597469f247f3b4561
Nandlal Jaiswal <nandlal.jaiswal.jaiswal@gmail.com>
2024-01-28 11:07:15 +0530

    change mongodb connection

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb://localhost:27017");
+        const connectionInstance = await mongoose.connect("mongodb+srv://nandlal:y9pZ58Wap3xLbbXC@cluster0.xgax1uu.mongodb.net/instagram?retryWrites=true&w=majority");
         console.log(`\n MongoDB connected !!`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
@@ -17,4 +17,6 @@ module.exports = connectDB;
 // mongodb://localhost:27017
 
 // mongodb altas server
+// password  y9pZ58Wap3xLbbXC
+// mongodb+srv://nandlal:y9pZ58Wap3xLbbXC@cluster0.xgax1uu.mongodb.net/instagram?retryWrites=true&w=majority
 // "mongodb+srv://nandlal:wvtbvkDhdmKiyzBy@cluster0.qaa1jk5.mongodb.net/instagramClone?retryWrites=true&w=majority"
